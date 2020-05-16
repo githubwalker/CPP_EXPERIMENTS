65f97d797b5e5e26b2f93fcc59de6f11d59e362d
andrew <alebedeff@yandex.ru>
2020-05-16 17:44:38 +0300

    some fixes/cleanups + one more algo for parallel summarizing

--- test_parallel/main.cpp
+++ test_parallel/main.cpp
@@ -3,16 +3,10 @@
 
 #include <algorithm>
 #include <random>
-#include <iostream>
-#include <functional>
 #include <future>
-#include <thread>
 #include <chrono>
-
-#include <random>
 #include <queue>
 
-#include "thread_pool.h"
 
 
 template <typename _ittype>
@@ -31,7 +25,7 @@ int parallel_sum(_ittype itbeg, _ittype itend)
 template <typename _ittype>
 int parallel_sum2(_ittype itbeg, _ittype itend, thread_pool::thread_pool& tp)
 {
-    const std::size_t min_step = 10000000;
+    const std::size_t min_step = 100000000;
     auto dist = itend - itbeg;
     if (dist < min_step)
         return std::accumulate(itbeg, itend, 0);
@@ -61,6 +55,77 @@ int parallel_sum2(_ittype itbeg, _ittype itend, thread_pool::thread_pool& tp)
     return total;
 }
 
+// try to make it cache friendly
+template <typename _ittype>
+int parallel_sum3(_ittype itbeg, _ittype itend)
+{
+    const std::size_t nBunchSize = 10000000; // 1024 * 1024;
+    const auto thcount = boost::thread::hardware_concurrency() * 2;
+    const std::size_t nItems = std::distance(itbeg, itend);
+
+    std::vector<std::future<int>> futs;
+
+    for(std::size_t iThread = 0; iThread < thcount; ++iThread)
+    {
+        futs.emplace_back(std::async( [itbeg, itend, nItems, thcount, iThread]()
+            {
+                int sum = 0;
+
+                for(std::size_t iLeft = iThread * nBunchSize; iLeft < nItems; iLeft += nBunchSize * thcount)
+                {
+                    std::size_t iRight = (std::min)(iLeft + nBunchSize, nItems);
+                    sum += std::accumulate(itbeg+iLeft, itbeg+iRight, 0);
+                }
+
+                return sum;
+            }));
+    }
+
+    int sum = 0;
+
+    for(auto & fut : futs)
+        sum += fut.get();
+
+    return sum;
+}
+
+
+// try to make it cache friendly
+template <typename _itemType>
+int parallel_sum4(const std::vector<_itemType>& vc)
+{
+    const std::size_t nBunchSize = 10000000; // 1024 * 1024;
+    const auto thcount = boost::thread::hardware_concurrency() * 2;
+
+    std::vector<std::future<int>> futs;
+
+    for(std::size_t iThread = 0; iThread < thcount; ++iThread)
+    {
+        futs.emplace_back(std::async( [&vc, thcount, iThread]()
+              {
+                  int sum = 0;
+                  std::size_t nItems = vc.size();
+
+                  for(std::size_t iLeft = iThread * nBunchSize; iLeft < nItems; iLeft += nBunchSize * thcount)
+                  {
+                      std::size_t iRight = (std::min)(iLeft + nBunchSize, nItems);
+                      for(std::size_t i = iLeft; i < iRight; i++)
+                           sum += vc[i];
+                  }
+
+                  return sum;
+              }));
+    }
+
+    int sum = 0;
+
+    for(auto & fut : futs)
+        sum += fut.get();
+
+    return sum;
+}
+
+
 struct MeasureScope
 {
     std::chrono::system_clock::time_point beg_;
@@ -81,39 +146,63 @@ void promisses_and_futures()
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist(1,100000000);
 
+    const std::size_t nrepeats = 100;
     const std::size_t nitems = 4000000000;
 
     std::vector<std::int16_t> vc;
     vc.reserve(nitems);
 
+    std::cout << "allocating " << nitems << " items " << std::endl;
+
     for(std::size_t i = 0; i < nitems; i ++)
         vc.emplace_back((std::int16_t)dist(rng));
 
-    std::cout << "Starting meqsurements" << std::endl;
+    std::cout << "Starting measurements" << std::endl;
 
+    int val = 0;
+
+    // single thread
     MeasureScope ms0;
-    auto val0 = parallel_sum(vc.begin(), vc.end());
-    std::cout << "Time spent in parallel (micros): " << ms0.GetMicros() << ", result is: " << val0 << std::endl;
+    for(std::size_t i=0; i < nrepeats; i++)
+        val = std::accumulate(vc.begin(), vc.end(), 0);
 
+    std::cout << "Time spent in single thread (micros): " << ms0.GetMicros() << ", result is: " << val << std::endl;
+
+    // parallel_sum1
     MeasureScope ms1;
-    auto val1 = std::accumulate(vc.begin(), vc.end(), 0);
-    std::cout << "Time spent in single thread (micros): " << ms1.GetMicros() << ", result is: " << val1 << std::endl;
+    for(std::size_t i=0; i < nrepeats; i++)
+        val = parallel_sum(vc.begin(), vc.end());
 
-    auto thcount = boost::thread::hardware_concurrency() * 4;
-    std::cout << "thcount = " << thcount << std::endl;
+    std::cout << "Time spent in parallel (micros): " << ms1.GetMicros() << ", result is: " << val << std::endl;
 
-    thread_pool::thread_pool tp(thcount);
-
+    // parallel_sum2
+    thread_pool::thread_pool tp(boost::thread::hardware_concurrency() * 2);
     MeasureScope ms2;
-    auto val2 = parallel_sum2(vc.begin(), vc.end(), tp);
-    std::cout << "Time spent in thread pool (micros): " << ms2.GetMicros() << ", result is: " << val2 << std::endl;
+    for(std::size_t i=0; i < nrepeats; i++)
+        val = parallel_sum2(vc.begin(), vc.end(), tp);
+       
+    std::cout << "Time spent in thread pool (micros): " << ms2.GetMicros() << ", result is: " << val << std::endl;
 
-    return;
+    // parallel_sum3
+    MeasureScope ms3;
+    for(std::size_t i=0; i < nrepeats; i++)
+        val = parallel_sum3(vc.begin(), vc.end());
+
+    std::cout << "Time spent in cache-friendly parallel sum (micros): " << ms3.GetMicros() << ", result is: " << val << std::endl;
+
+
+    // parallel_sum4
+    MeasureScope ms4;
+    for(std::size_t i=0; i < nrepeats; i++)
+        val = parallel_sum4(vc);
+
+    std::cout << "Time spent in cache-friendly parallel sum (modified) (micros): " << ms4.GetMicros() << ", result is: " << val << std::endl;
 }
 
 
 int main(int argc, char** argv)
 {
+    std::cout << "size of std::size_t: " << sizeof(std::size_t) << std::endl;
     promisses_and_futures();
     return 0;
 }
